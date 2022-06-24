ad2e9981aa326d394fc5699f4ba1387c978bf21d
Ethan <ethanbaird94@gmail.com>
2022-06-24 16:05:42 +0100

    pass test 7

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -17,3 +17,9 @@ def get_pets_sold(pet_shop):
 
 def increase_pets_sold(pet_shop, sold_pets):
     pet_shop["admin"]["pets_sold"] += sold_pets
+
+def get_stock_count(pet_shop):
+    stock_count = 0
+    for pets in pet_shop["pets"]:
+        stock_count += 1
+    return stock_count
