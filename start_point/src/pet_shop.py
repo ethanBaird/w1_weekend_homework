9ab6ab488827690953dfbbdf712ef07b5915c2eb
Ethan <ethanbaird94@gmail.com>
2022-06-24 15:54:10 +0100

    test 3 pass

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -7,3 +7,7 @@ def get_pet_shop_name(pet_shop):
 def get_total_cash(pet_shop):
     total_cash = pet_shop["admin"]["total_cash"]
     return total_cash
+
+def add_or_remove_cash(pet_shop, cash):
+    pet_shop["admin"]["total_cash"] += cash
+    
