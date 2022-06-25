f1bc16a024e7c770f256be897c950b4a80db3eb6
Ethan <ethanbaird94@gmail.com>
2022-06-25 16:42:43 +0100

    pass test 19

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -60,6 +60,9 @@ def add_pet_to_customer(customer, new_pet):
     customer["pets"].append(new_pet)
 
 def customer_can_afford_pet(customer, new_pet):
-    if customer["cash"] >= new_pet["price"]: return True
+    if customer["cash"] >= new_pet["price"]: 
+        return True 
+    else: 
+        return False
         
     
